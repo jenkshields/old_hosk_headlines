22a884413df26cdd23a1500f5a4bd068bc31e2d6
jenkshields <jennifer@jenniferkshields.nz>
2018-06-28 13:59:31 +1200

    Added user input to hosking_bot.py to generate multiple outputs in one session... Added to source libraries.

--- hosking_bot.py
+++ hosking_bot.py
@@ -7,11 +7,23 @@ dirname = os.path.dirname(os.path.abspath(__file__))
 headline = os.path.join(dirname, 'hosking_headlines.txt')
 comment = os.path.join(dirname, 'hosking_comments.txt')
 
-tweetbot.read(headline)
-gen_head = tweetbot.generate_text(10)
-print(u'\nMike Hosking: %s' % (gen_head))
+gen = input('Generate headline? Y/N\n')
+while True:
+    if gen == 'Y' or gen == 'y':
+        tweetbot.read(headline)
+        gen_head = tweetbot.generate_text(10)
+        print(u'\nMike Hosking: %s' % (gen_head))
 
-tweetbot.clear_data()
-tweetbot.read(comment)
-gen_comm = tweetbot.generate_text(30)
-print('COMMENT: %s' % (gen_comm))
+        tweetbot.clear_data()
+        tweetbot.read(comment)
+        gen_comm = tweetbot.generate_text(30)
+        print('COMMENT: %s' % (gen_comm))
+        gen = input(u'\nGenerate headline? Y/N\n')
+        break
+
+    elif gen == 'N' or gen == 'n':
+        exit()
+
+    elif gen != 'Y' and gen != 'N' and gen != 'y' and gen != 'n':
+        print(u'\nInput was not Y or N')
+        gen = input(u'\nGenerate headline? Y/N\n')
